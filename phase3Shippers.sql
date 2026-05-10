40edf848490805ecf66919737dde0e06d76f6337
WevinKolfe <133042901+WevinKolfe@users.noreply.github.com>
2026-05-09 22:07:32 -0400

    Delete phase3Shippers.sql

--- phase3Shippers.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: select * from shippers
-LIMIT 0, 1000
-
--- Date: 2026-05-09 21:56
-*/
-INSERT INTO `` (`shipperID`) VALUES (3);
-INSERT INTO `` (`shipperID`) VALUES (1);
-INSERT INTO `` (`shipperID`) VALUES (2);
